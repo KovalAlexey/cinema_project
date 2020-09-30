43fa889abb7441e301de31e7fa386a7332df2879
Alexey <gradiviro@gmail.com>
2020-09-30 15:11:24 +0300

    configure hibernate, add models, dao and service

---
+++ src/main/java/com/dev/cinema/exceptions/DataProcesingException.java
@@ -0,0 +1,7 @@
+package com.dev.cinema.exceptions;
+
+public class DataProcesingException extends RuntimeException {
+    public DataProcesingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
