b3e952810fe3ef61a8005fbf8ef52555f5b8550f
Alexey <gradiviro@gmail.com>
2020-10-07 18:10:35 +0300

    draft version

---
+++ src/main/java/com/dev/cinema/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,36 @@
+package com.dev.cinema.service.impl;
+
+import com.dev.cinema.exceptions.AuthenticationException;
+import com.dev.cinema.lib.Inject;
+import com.dev.cinema.lib.Service;
+import com.dev.cinema.model.User;
+import com.dev.cinema.service.AuthenticationService;
+import com.dev.cinema.service.UserService;
+import com.dev.cinema.util.HashUtil;
+import java.util.Optional;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> user = userService.findByEmail(email);
+        if (user.isPresent() && user.get().getPassword()
+                .equals(HashUtil.hashPassword(password, user.get().getSalt()))) {
+            return user.get();
+        }
+        throw new AuthenticationException("Incorrect username or password!");
+    }
+
+    @Override
+    public User register(String email, String password) {
+        User user = new User();
+        byte[] salt = HashUtil.getSalt();
+        user.setPassword(HashUtil.hashPassword(password, salt));
+        user.setSalt(salt);
+        user.setEmail(email);
+        return userService.add(user);
+    }
+}
