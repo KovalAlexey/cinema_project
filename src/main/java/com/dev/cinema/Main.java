a7441c3d95fd8ac37e7fbe58acc593bb04cb78d6
Alexey <gradiviro@gmail.com>
2020-10-12 12:00:19 +0300

    ready for review

--- src/main/java/com/dev/cinema/Main.java
+++ src/main/java/com/dev/cinema/Main.java
@@ -6,53 +6,52 @@ import com.dev.cinema.model.CinemaHall;
 import com.dev.cinema.model.Movie;
 import com.dev.cinema.model.MovieSession;
 import com.dev.cinema.model.User;
-import com.dev.cinema.service.AuthenticationService;
 import com.dev.cinema.service.CinemaHallService;
 import com.dev.cinema.service.MovieService;
 import com.dev.cinema.service.MovieSessionService;
+import com.dev.cinema.service.ShoppingCartService;
 import com.dev.cinema.service.UserService;
 import java.time.LocalDateTime;
 
 public class Main {
     private static Injector injector = Injector.getInstance("com.dev.cinema");
 
+    static final MovieService movieService
+            = (MovieService) injector.getInstance(MovieService.class);
+    static final CinemaHallService cinemaHallService
+            = (CinemaHallService) injector.getInstance(CinemaHallService.class);
+    static final MovieSessionService sessionService
+            = (MovieSessionService) injector.getInstance(MovieSessionService.class);
+    static final UserService userService
+            = (UserService) injector.getInstance(UserService.class);
+    static final ShoppingCartService shoppingCartService
+            = (ShoppingCartService) injector.getInstance(ShoppingCartService.class);
+
     public static void main(String[] args) throws AuthenticationException {
         Movie movie = new Movie();
         movie.setTitle("Fast and Furious");
-
-        CinemaHall red = new CinemaHall();
-        red.setCapacity(100);
-        red.setDescription("IMAX grand hall");
-
-        LocalDateTime today = LocalDateTime.now();
-        MovieSession premierSession = new MovieSession();
-        premierSession.setCinemaHall(red);
-        premierSession.setLocalDateTime(today);
-        premierSession.setMovie(movie);
-
-        MovieService movieService =
-                (MovieService) injector.getInstance(MovieService.class);
-        CinemaHallService cinemaHallService =
-                (CinemaHallService) injector.getInstance(CinemaHallService.class);
-        MovieSessionService movieSessionService =
-                (MovieSessionService) injector.getInstance(MovieSessionService.class);
         movieService.add(movie);
-        cinemaHallService.add(red);
-        movieSessionService.add(premierSession);
 
-        movieService.getAll().forEach(System.out::println);
-        cinemaHallService.getAll().forEach(System.out::println);
-        movieSessionService.findAvailableSessions(1L, today.toLocalDate())
-                .forEach(System.out::println);
 
-        AuthenticationService authService =
-                (AuthenticationService) injector.getInstance(AuthenticationService.class);
+        CinemaHall cinemaHall = new CinemaHall();
+        cinemaHall.setCapacity(100);
+        cinemaHall.setDescription("IMAX");
+        cinemaHallService.add(cinemaHall);
+
+        MovieSession session = new MovieSession();
+        session.setCinemaHall(cinemaHall);
+        session.setMovie(movie);
+        session.setShowTime(LocalDateTime.now());
+        sessionService.add(session);
+
         User user = new User();
         user.setEmail("test@test.com");
         user.setPassword("1234");
-        authService.register(user.getEmail(), user.getPassword());
-        authService.login(user.getEmail(), user.getPassword());
-        UserService userService = (UserService) injector.getInstance(UserService.class);
-        System.out.println(userService.findByEmail(user.getEmail()).get());
+        userService.add(user);
+
+        shoppingCartService.registerNewShoppingCart(user);
+        shoppingCartService.addSession(session, user);
+        System.out.println(shoppingCartService.getByUser(user));
     }
 }
+
---
+++ src/main/java/com/dev/cinema/model/ShoppingCart.java
@@ -0,0 +1,53 @@
+package com.dev.cinema.model;
+
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.MapsId;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "shopping_carts")
+public class ShoppingCart {
+    @Id
+    @Column(name = "cart_id")
+    private Long id;
+    @OneToMany
+    @JoinTable(name = "tickets_cart",
+            joinColumns = @JoinColumn(name = "cart_id"),
+            inverseJoinColumns = @JoinColumn(name = "ticket_id"))
+    private List<Ticket> tickets;
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
+}
